f3f18a2cdfc1ff773b643e879683bc98c1f9d152
angleshe <478647464@qq.com>
2024-03-09 02:25:48 +0800

    feat: 生成ast

--- packages/compiler-core/src/ast.ts
+++ packages/compiler-core/src/ast.ts
@@ -1,1 +1,19 @@
-export interface RootNode {}
+export interface Node {
+  type: NodeType;
+}
+
+export interface TextNode extends Node {
+  type: NodeType.TEXT;
+  content: string;
+}
+
+export type TemplateChildNode = TextNode;
+export interface RootNode extends Node {
+  type: NodeType.ROOT;
+  children: TemplateChildNode[];
+}
+
+export enum NodeType {
+  ROOT,
+  TEXT,
+}
--- packages/compiler-core/src/parse.ts
+++ packages/compiler-core/src/parse.ts
@@ -1,5 +1,57 @@
-import type { RootNode } from './ast';
+import {
+  type RootNode,
+  NodeType,
+  type TemplateChildNode,
+  TextNode,
+} from './ast';
+
+interface ParserContext {
+  source: string;
+}
+
+function advanceBy(context: ParserContext, numberOfCharacters: number) {
+  const { source } = context;
+  context.source = source.slice(numberOfCharacters);
+}
+
+function isEnd(context: ParserContext): boolean {
+  const s = context.source;
+  return !s;
+}
+
+function parseTextData(context: ParserContext, length: number): string {
+  const rawText = context.source.slice(0, length);
+  advanceBy(context, length);
+  return rawText;
+}
+
+function parseText(context: ParserContext): TextNode {
+  const content = parseTextData(context, context.source.length);
+  return {
+    type: NodeType.TEXT,
+    content,
+  };
+}
+
+function parseChildren(context: ParserContext): TemplateChildNode[] {
+  const nodes: TemplateChildNode[] = [];
+  while (!isEnd(context)) {
+    const node = parseText(context);
+    nodes.push(node);
+  }
+  return nodes;
+}
+
+function createParseContext(content: string): ParserContext {
+  return {
+    source: content,
+  };
+}
+
 export function parse(content: string): RootNode {
-  console.log('parse===>', content);
-  return {};
+  const context = createParseContext(content);
+  return {
+    type: NodeType.ROOT,
+    children: parseChildren(context),
+  };
 }
